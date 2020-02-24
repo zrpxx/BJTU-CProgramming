36a12077929d0f35024452d96805dd0dbd52929d
zrpxx <zrpxx@qq.com>
2020-02-24 18:12:03 +0800

    Inital. Added Helloworld.c

---
+++ cmake-build-debug/CMakeFiles/Learn.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Learn.dir/main.c.obj"
+  "Learn.exe"
+  "Learn.exe.manifest"
+  "Learn.pdb"
+  "libLearn.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Learn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
