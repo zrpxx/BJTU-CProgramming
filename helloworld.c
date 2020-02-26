e7a0718b5d6bd668409f9f8f496cabfb0104ab29
zrpxx <zrpxx@qq.com>
2020-02-26 16:38:02 +0800

    Updated helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -1,6 +1,12 @@
+/**
+* @Author Trevor.Z
+* @Date：2020/02/04
+* @Desc:Helloworld例程
+*/
+
 #include <stdio.h>
 
 int main() {
-    printf("Hello world!\n");
+    printf("Hello world!\n"); // \n = 换行
     return 0;
 }
